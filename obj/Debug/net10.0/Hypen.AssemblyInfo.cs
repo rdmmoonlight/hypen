98c4263ade5057943c73adc49defcd7b3d0f57f6
rdmmoonlight <286575778+rdmmoonlight@users.noreply.github.com>
2026-08-12 02:42:07 +0000

    style(ci): auto-fix code style and linting

--- obj/Debug/net10.0/Hypen.AssemblyInfo.cs
+++ obj/Debug/net10.0/Hypen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hypen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116a64b99aaaebfe770d253b7848629cdb252378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976b3f7d0813cb1a5d93f2b23655168a10c14512")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hypen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hypen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
