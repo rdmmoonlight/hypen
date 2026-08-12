208ec6d0893bf79c153b60a7f51eb315f46a8322
Obscuron <ndopoer@gmail.com>
2026-08-12 08:27:35 +0700

    Delete coi-serviceworker.js

--- coi-serviceworker.js
+++
@@ -1,8 +0,0 @@
-// Sourced from https://github.com/gzuidhof/coi-serviceworker
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./coi-serviceworker.js').then((registration) => {
-    registration.addEventListener('updatefound', () => {
-      window.location.reload();
-    });
-  });
-                                  }
