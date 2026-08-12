4cd9eb4ec7f1abff687faa4af2ee7fd9d47f8665
rdmmoonlight <286575778+rdmmoonlight@users.noreply.github.com>
2026-08-12 06:08:15 +0000

    style(ci): auto-fix code style and linting [skip ci]

--- apps/mobile/obj/Debug/net10.0-android/HypenMaui.AssemblyInfo.cs
+++ apps/mobile/obj/Debug/net10.0-android/HypenMaui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HypenMaui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32eddafb459a7274cbffd6f3474379a5ae72fd1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4e3dcb7d2d9070f34c081c9c210cf1106a0afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HypenMaui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HypenMaui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
