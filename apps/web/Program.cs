c7411871a682a03de71a527c18605537e865dac3
Obscuron <ndopoer@gmail.com>
2026-08-12 13:39:54 +0700

    Register SongService in Program.cs
    Added SongService registration to the service collection.

--- apps/web/Program.cs
+++ apps/web/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Hypen.Web;
+using Hypen.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Registrasi HttpClient mengarah ke URL API Render
+// Base Address mengarah ke Backend Render
 string backendUrl = builder.Configuration["BACKEND_URL"] ?? "https://hypen-0s65.onrender.com";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(backendUrl) });
+
+// Registrasi SongService
+builder.Services.AddScoped<ISongService, SongService>();
 
 await builder.Build().RunAsync();
