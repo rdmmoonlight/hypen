32acbf5ebf4d1cf3c02df2ba78ee898c419a255f
Obscuron <ndopoer@gmail.com>
2026-08-12 13:28:32 +0700

    Add ISongService interface for song management

---
+++ apps/web/Services/ISongService.cs
@@ -0,0 +1,13 @@
+using Hypen.Web.Models;
+
+namespace Hypen.Web.Services;
+
+public interface ISongService
+{
+    Task<List<SongModel>> GetSongsAsync();
+    Task<ConvertResponse?> ConvertVideoAsync(string youtubeUrl);
+    Task<PlaylistResponse?> ConvertPlaylistAsync(string playlistUrl);
+    Task<bool> DeleteSongAsync(int id);
+    Task<bool> DeleteBatchSongsAsync(int[] ids);
+    Task DownloadSongAsync(string audioUrl, string title);
+}
