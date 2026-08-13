7c9d60bcd63ebe702cf924eecbef02000ead5802
Obscuron <ndopoer@gmail.com>
2026-08-13 11:52:48 +0700

    Register navigation routes for MainPage and SettingsPage

--- apps/mobile/AppShell.xaml.cs
+++ apps/mobile/AppShell.xaml.cs
@@ -1,3 +1,7 @@
+using HypenMaui.Pages.Home;
+using HypenMaui.Pages.Settings;
+using Microsoft.Maui.Controls;
+
 namespace HypenMaui;
 
 public partial class AppShell : Shell
@@ -5,5 +9,9 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+
+        // Registrasi Route Navigasi
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
     }
 }
