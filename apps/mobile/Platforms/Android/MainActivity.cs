3e054a9c21bf11651d0b275648f5d6462743775d
Obscuron <ndopoer@gmail.com>
2026-08-13 10:40:57 +0700

    Format ConfigurationChanges for better readability

--- apps/mobile/Platforms/Android/MainActivity.cs
+++ apps/mobile/Platforms/Android/MainActivity.cs
@@ -8,7 +8,12 @@ namespace HypenMaui;
     Theme = "@style/Maui.SplashTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
-    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    ConfigurationChanges = ConfigChanges.ScreenSize 
+        | ConfigChanges.Orientation 
+        | ConfigChanges.UiMode 
+        | ConfigChanges.ScreenLayout 
+        | ConfigChanges.SmallestScreenSize 
+        | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
