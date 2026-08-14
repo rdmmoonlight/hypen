86045dc759e37468f8c3aeb00b6a6d617b29ed99
Obscuron <ndopoer@gmail.com>
2026-08-14 15:50:10 +0700

    Register route for LibraryPage in AppShell

--- apps/mobile/AppShell.xaml.cs
+++ apps/mobile/AppShell.xaml.cs
@@ -1,4 +1,5 @@
 using HypenMaui.Pages.Home;
+using HypenMaui.Pages.Library;
 using HypenMaui.Pages.NowPlaying;
 using HypenMaui.Pages.Settings;
 using Microsoft.Maui.Controls;
@@ -13,6 +14,7 @@ public partial class AppShell : Shell
 
         // Registrasi Route Navigasi
         Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(LibraryPage), typeof(LibraryPage));
         Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
         Routing.RegisterRoute(nameof(NowPlayingPage), typeof(NowPlayingPage));
     }
