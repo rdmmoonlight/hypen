ead5d588e538dc0788880470b249c95c55e97ce1
Obscuron <ndopoer@gmail.com>
2026-08-19 05:34:36 +0700

    Add LocalMp3ExtractModel class for MP3 metadata

---
+++ Models/LocalMp3ExtractModel.cs
@@ -0,0 +1,18 @@
+namespace Hypen.Web.Models;
+
+public class LocalMp3ExtractModel
+{
+    public string FileName { get; set; } = string.Empty;
+    public string RawArtist { get; set; } = string.Empty;
+    public string RawTitle { get; set; } = string.Empty;
+    
+    // Metadata Hasil Cleanup
+    public string CleanArtist { get; set; } = string.Empty;
+    public string CleanTitle { get; set; } = string.Empty;
+    public string Album { get; set; } = "Single";
+    public int? ReleaseYear { get; set; }
+    public string AlbumCoverUrl { get; set; } = string.Empty;
+
+    public bool IsSelected { get; set; } = true;
+    public bool IsProcessed { get; set; }
+}
