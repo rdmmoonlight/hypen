4a14b5efb3c91c9268972579d017ce30063e016d
Obscuron <ndopoer@gmail.com>
2026-08-19 15:30:57 +0700

    Add global using directives in Usings.cs

---
+++ Usings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Hypen.Web.Data;
+global using Hypen.Web.Models;
+global using Hypen.Web.Services;
