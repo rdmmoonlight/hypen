74f36ec9f0c9fc5b5fc3b3eb3d99b7f6b7012f86
Obscuron <ndopoer@gmail.com>
2026-08-20 11:03:49 +0700

    Add ModelAliases for backward compatibility

---
+++ Models/ModelAliases.cs
@@ -0,0 +1,8 @@
+namespace Hypen.Web.Models;
+
+/// <summary>
+/// Alias turunan untuk kompatibilitas kode lama tanpa breaking change
+/// </summary>
+public class RawSongModel : SongModel { }
+
+public class CloudSongModel : SongModel { }
