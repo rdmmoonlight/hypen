7918d550c6bad86348596adeaa6518a421262981
Obscuron <ndopoer@gmail.com>
2026-08-24 10:38:37 +0700

    Remove empty OnInitializedAsync method

--- Components/Pages/Tools/Index.razor.cs
+++ Components/Pages/Tools/Index.razor.cs
@@ -4,8 +4,4 @@ namespace Hypen.Web.Components.Pages.Tools;
 
 public partial class Index : ComponentBase
 {
-    protected override async Task OnInitializedAsync()
-    {
-        await Task.CompletedTask;
-    }
 }
