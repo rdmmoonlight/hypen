5f2f313577433ca6b7e4a0de72caa47fd13df63d
Obscuron <ndopoer@gmail.com>
2026-08-24 10:08:51 +0700

    Implement tools submenu toggle in Sidebar component
    Added functionality to manage tools submenu state when sidebar is collapsed.

--- Components/Layout/Sidebar.razor.cs
+++ Components/Layout/Sidebar.razor.cs
@@ -5,6 +5,7 @@ namespace Hypen.Web.Components
     public partial class Sidebar
     {
         private bool isExpanded = true;
+        private bool isToolsSubmenuOpen = false;
 
         [Parameter]
         public EventCallback<bool> OnToggle { get; set; }
@@ -16,10 +17,29 @@ namespace Hypen.Web.Components
         {
             isExpanded = !isExpanded;
 
+            if (!isExpanded)
+            {
+                isToolsSubmenuOpen = false; // Otomatis tutup submenu jika sidebar di-collapse
+            }
+
             if (OnToggle.HasDelegate)
             {
                 await OnToggle.InvokeAsync(isExpanded);
             }
+        }
+
+        private async Task ToggleToolsSubmenu()
+        {
+            if (!isExpanded)
+            {
+                isExpanded = true;
+                if (OnToggle.HasDelegate)
+                {
+                    await OnToggle.InvokeAsync(isExpanded);
+                }
+            }
+
+            isToolsSubmenuOpen = !isToolsSubmenuOpen;
         }
 
         protected void NavigateToLibrary()
@@ -40,6 +60,21 @@ namespace Hypen.Web.Components
         protected void NavigateToTools()
         {
             Navigation.NavigateTo("/tools");
+        }
+
+        protected void NavigateToDuplicateDetector()
+        {
+            Navigation.NavigateTo("/tools/drivedetector");
+        }
+
+        protected void NavigateToLocalSync()
+        {
+            Navigation.NavigateTo("/tools/localsync");
+        }
+
+        protected void NavigateToGDriveManagement()
+        {
+            Navigation.NavigateTo("/tools/gdrive-management");
         }
 
         protected void NavigateToSetting()
