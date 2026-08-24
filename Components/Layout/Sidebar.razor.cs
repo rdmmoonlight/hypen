3269c4eb1b212e3574636f24f4dcfaee2c810691
Obscuron <ndopoer@gmail.com>
2026-08-24 10:44:31 +0700

    Refactor Sidebar component for clarity and efficiency

--- Components/Layout/Sidebar.razor.cs
+++ Components/Layout/Sidebar.razor.cs
@@ -1,85 +1,84 @@
 using Microsoft.AspNetCore.Components;
 
-namespace Hypen.Web.Components
+namespace Hypen.Web.Components;
+
+public partial class Sidebar
 {
-    public partial class Sidebar
+    private bool isExpanded = true;
+    private bool isToolsSubmenuOpen = false;
+
+    [Parameter]
+    public EventCallback<bool> OnToggle { get; set; }
+
+    [Inject]
+    protected NavigationManager Navigation { get; set; } = default!;
+
+    private async Task ToggleSidebar()
     {
-        private bool isExpanded = true;
-        private bool isToolsSubmenuOpen = false;
+        isExpanded = !isExpanded;
 
-        [Parameter]
-        public EventCallback<bool> OnToggle { get; set; }
-
-        [Inject]
-        protected NavigationManager Navigation { get; set; } = default!;
-
-        private async Task ToggleSidebar()
+        if (!isExpanded)
         {
-            isExpanded = !isExpanded;
+            isToolsSubmenuOpen = false; // Otomatis tutup submenu jika sidebar di-collapse
+        }
 
-            if (!isExpanded)
-            {
-                isToolsSubmenuOpen = false; // Otomatis tutup submenu jika sidebar di-collapse
-            }
+        if (OnToggle.HasDelegate)
+        {
+            await OnToggle.InvokeAsync(isExpanded);
+        }
+    }
 
+    private async Task ToggleToolsSubmenu()
+    {
+        if (!isExpanded)
+        {
+            isExpanded = true;
             if (OnToggle.HasDelegate)
             {
                 await OnToggle.InvokeAsync(isExpanded);
             }
         }
 
-        private async Task ToggleToolsSubmenu()
-        {
-            if (!isExpanded)
-            {
-                isExpanded = true;
-                if (OnToggle.HasDelegate)
-                {
-                    await OnToggle.InvokeAsync(isExpanded);
-                }
-            }
+        isToolsSubmenuOpen = !isToolsSubmenuOpen;
+    }
 
-            isToolsSubmenuOpen = !isToolsSubmenuOpen;
-        }
+    protected void NavigateToLibrary()
+    {
+        Navigation.NavigateTo("/library");
+    }
 
-        protected void NavigateToLibrary()
-        {
-            Navigation.NavigateTo("/library");
-        }
+    protected void NavigateToSync()
+    {
+        Navigation.NavigateTo("/extraction");
+    }
 
-        protected void NavigateToSync()
-        {
-            Navigation.NavigateTo("/extraction");
-        }
+    protected void NavigateToStaging()
+    {
+        Navigation.NavigateTo("/staging");
+    }
 
-        protected void NavigateToStaging()
-        {
-            Navigation.NavigateTo("/staging");
-        }
+    protected void NavigateToTools()
+    {
+        Navigation.NavigateTo("/tools");
+    }
 
-        protected void NavigateToTools()
-        {
-            Navigation.NavigateTo("/tools");
-        }
+    protected void NavigateToDuplicateDetector()
+    {
+        Navigation.NavigateTo("/tools/dedup");
+    }
 
-        protected void NavigateToDuplicateDetector()
-        {
-            Navigation.NavigateTo("/tools/dedup");
-        }
+    protected void NavigateToLocalSync()
+    {
+        Navigation.NavigateTo("/tools/synclocal");
+    }
 
-        protected void NavigateToLocalSync()
-        {
-            Navigation.NavigateTo("/tools/localsync");
-        }
+    protected void NavigateToGDriveManagement()
+    {
+        Navigation.NavigateTo("/tools/drivedetector");
+    }
 
-        protected void NavigateToGDriveManagement()
-        {
-            Navigation.NavigateTo("/tools/drivedetector");
-        }
-
-        protected void NavigateToSetting()
-        {
-            Navigation.NavigateTo("/settingss");
-        }
+    protected void NavigateToSetting()
+    {
+        Navigation.NavigateTo("/settingss");
     }
 }
