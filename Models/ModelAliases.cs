b6cd53f6c987f86a9efb692c536cd53e95ca6a7a
Obscuron <ndopoer@gmail.com>
2026-08-25 09:46:37 +0700

    Delete Models/ModelAliases.cs

--- Models/ModelAliases.cs
+++
@@ -1,8 +0,0 @@
-namespace Hypen.Web.Models;
-
-/// <summary>
-/// Alias turunan untuk kompatibilitas kode lama tanpa breaking change
-/// </summary>
-public class RawSongsModel : SongsModel { }
-
-public class CloudSongsModel : SongsModel { }
