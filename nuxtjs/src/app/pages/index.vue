f9a820b35633d220210e51e63596567fbd3cbc37
Obscuron <ndopoer@gmail.com>
2026-09-17 10:58:33 +0700

    Update index.vue

--- nuxtjs/src/app/pages/index.vue
+++ nuxtjs/src/app/pages/index.vue
@@ -1,29 +1,28 @@
 <!-- pages/index.vue -->
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
 const handleEnter = () => {
   navigateTo('/home')
 }
 </script>
 
 <template>
-  <div>
-    <!-- Nuxt Welcome Screen Bawaan -->
-    <NuxtWelcome />
-
-    <!-- Teleport ke body untuk melepaskan diri dari CSS NuxtWelcome -->
-    <ClientOnly>
-      <Teleport to="body">
-        <div class="fixed top-6 right-6 z-[999999]">
-          <Button 
-            size="lg" 
-            class="shadow-2xl hover:scale-105 transition-all gap-2 cursor-pointer"
-            @click="handleEnter"
-          >
-            <span>Masuk ke Dalam Web</span>
+  <div class="relative min-h-screen">
+    <!-- Menyuntikkan tombol ke dalam slot default NuxtWelcome -->
+    <NuxtWelcome>
+      <div class="fixed top-6 right-6 z-[999999] pointer-events-auto">
+        <Button 
+          size="lg" 
+          class="bg-emerald-500 hover:bg-emerald-600 text-black font-semibold px-6 shadow-xl transition-all hover:scale-105 gap-2 cursor-pointer relative"
+          @click="handleEnter"
+        >
+          <span>Masuk ke Dalam Web</span>
+          <ClientOnly>
             <Icon name="tabler:arrow-right" class="w-5 h-5" />
-          </Button>
-        </div>
-      </Teleport>
-    </ClientOnly>
+          </ClientOnly>
+        </Button>
+      </div>
+    </NuxtWelcome>
   </div>
 </template>
