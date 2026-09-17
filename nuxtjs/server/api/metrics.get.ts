cbeef914766c5e856d681e22a8ca5d082cf71328
Obscuron <ndopoer@gmail.com>
2026-09-17 13:41:41 +0700

    Delete nuxtjs/server/api/metrics.get.ts

--- nuxtjs/server/api/metrics.get.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '../utils/prisma'
-export default defineEventHandler(async () => {
-  const [rawCount, completedCount] = await Promise.all([
-    prisma.rawSongs.count(),
-    prisma.song.count()
-  ])
-  return { rawCount, completedCount }
-})
